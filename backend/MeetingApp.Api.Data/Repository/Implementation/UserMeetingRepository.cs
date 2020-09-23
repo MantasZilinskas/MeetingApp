2c29342033ef79aa7848ca04a28e78396b0d7187
Mantas Žilinskas <43217237+MantasZilinskas@users.noreply.github.com>
2020-09-23 18:40:56 +0300

    Created UserMeeting repository

---
+++ backend/MeetingApp.Api.Data/Repository/Implementation/UserMeetingRepository.cs
@@ -0,0 +1,43 @@
+﻿using MeetingApp.Api.Data.Context;
+using MeetingApp.Api.Data.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Linq;
+using MeetingApp.Api.Data.Model;
+
+namespace MeetingApp.Api.Data.Repository.Implementation
+{
+    public class UserMeetingRepository : IUserMeetingRepository
+    {
+        private readonly MeetingAppContext _context;
+
+        public UserMeetingRepository(MeetingAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Delete(int meetingId)
+        {
+            var entitiesToRemove = await _context.UserMeetings.Where(entity => entity.MeetingId == meetingId).ToListAsync();
+            _context.UserMeetings.RemoveRange(entitiesToRemove);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Insert(List<int> userIds, int meetingId)
+        {
+            foreach(var userId in userIds)
+            {
+                _context.Add(new UserMeeting { MeetingId = meetingId, UserId = userId });
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(List<int> userIds, int meetingId)
+        {
+            await Delete(meetingId);
+            await Insert(userIds, meetingId);
+        }
+    }
+}
---
+++ backend/MeetingApp.Api.Data/Repository/Interfaces/IUserMeetingRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingApp.Api.Data.Repository.Interfaces
+{
+    public interface IUserMeetingRepository
+    {
+        public Task Insert(List<int> userIds, int meetingId);
+        public Task Update(List<int> userIds, int meetingId);
+        public Task Delete(int meetingId);
+    }
+}
