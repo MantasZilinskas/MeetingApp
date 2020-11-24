bdb938a1c0ff0e4d479f2baf952473e63050afc9
Mantas Žilinskas <43217237+MantasZilinskas@users.noreply.github.com>
2020-11-24 11:59:32 +0200

    Implemented routing functionality

--- frontend/src/Routes/Routes.js
+++ frontend/src/Routes/Routes.js
@@ -1,10 +1,13 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import SignIn from "../components/SignIn";
 
 export default function Routes(){
     return (
         <Switch>
             <Route path="/signin" component={SignIn} />
+            <Route path="/">
+                <Redirect to="/signin"/>
+            </Route>
         </Switch>
     );
 }
