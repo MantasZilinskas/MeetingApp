7b2af6dd3b1c3569324cf4a0cc49710258d1a7df
Mantas Žilinskas <43217237+MantasZilinskas@users.noreply.github.com>
2020-11-24 15:30:15 +0200

    Created signup template

--- frontend/src/Routes/Routes.js
+++ frontend/src/Routes/Routes.js
@@ -1,13 +1,15 @@
-import { Switch, Route, Redirect } from "react-router-dom";
-import SignIn from "../components/SignIn";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import SignIn from '../components/SignIn';
+import SignUp from '../components/SignUp';
 
-export default function Routes(){
-    return (
-        <Switch>
-            <Route path="/signin" component={SignIn} />
-            <Route path="/">
-                <Redirect to="/signin"/>
-            </Route>
-        </Switch>
-    );
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/signin" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/">
+        <Redirect to="/signin" />
+      </Route>
+    </Switch>
+  );
 }
