d1da71d67bd801cb5463aa1091923d7791ebd277
Mantas Žilinskas <43217237+MantasZilinskas@users.noreply.github.com>
2020-11-24 16:44:37 +0200

    User list template

--- frontend/src/Routes/Routes.js
+++ frontend/src/Routes/Routes.js
@@ -1,12 +1,14 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
 import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
+import UserListAdmin from '../components/UserListAdmin';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/signin" component={SignIn} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/userListAdmin" component={UserListAdmin} />
       <Route path="/">
         <Redirect to="/signin" />
       </Route>
--- frontend/src/components/Navigation/Navbar.js
+++ frontend/src/components/Navigation/Navbar.js
@@ -4,35 +4,25 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-//import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import { CardMedia } from '@material-ui/core';
-import PropTypes from 'prop-types';
 
-const useStyles = makeStyles(() => ({
-}));
+const useStyles = makeStyles(() => ({}));
 
-const Navbar = () => {
+export default function Navbar() {
   const classes = useStyles();
 
   return (
     <AppBar position="sticky" className={classes.bar}>
       <Toolbar className={classes.toolbar}>
-        <Button
-          component={NavLink}
-          to="/signup"
-        >
+        <Button component={NavLink} to="/signup">
           SignUp
         </Button>
-        <Button
-          component={NavLink}
-          to="/signin"
-        >
+        <Button component={NavLink} to="/signin">
           SignIn
+        </Button>
+        <Button component={NavLink} to="/userListAdmin">
+          UserListAdmin
         </Button>
       </Toolbar>
     </AppBar>
   );
-};
-
-export default Navbar;
+}
