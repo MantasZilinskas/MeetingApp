f8b9a9965d9cd3d6d6d79a2ceb3025910a825769
Mantas Žilinskas <43217237+MantasZilinskas@users.noreply.github.com>
2020-12-06 20:28:59 +0200

    Transformed navbar to responsive menu

--- frontend/src/components/Navigation/Navbar.js
+++ frontend/src/components/Navigation/Navbar.js
@@ -1,52 +1,98 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import { createMuiTheme, makeStyles } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
 import { Role } from '../../Utils/Role';
+import ResponsiveMenu from 'react-responsive-navbar';
+import { Box } from '@material-ui/core';
+import MenuOpenIcon from '@material-ui/icons/MenuOpen';
+import CloseIcon from '@material-ui/icons/Close';
 
-const useStyles = makeStyles(() => ({}));
+const useStyles = makeStyles((theme) => ({
+  largeMenu: {
+    backgroundColor: '#3F51B5',
+  },
+  link: {
+    color: 'white',
+  },
+  smallMenu: {
+    backgroundColor: '#3F51B5',
+  },
+  icon: {
+    marginLeft: theme.spacing(2),
+    display: 'flex',
+    alignContent: 'flex-end',
+    fontSize: 36,
+    color: 'white',
+  },
+}));
 
 export default function Navbar({ currentUser, setCurrentUser }) {
-  console.log(currentUser);
   const classes = useStyles();
-  return (
-    <AppBar position="sticky" className={classes.bar}>
-      <Toolbar className={classes.toolbar}>
-        {currentUser !== null && currentUser.roles.includes(Role.Admin) && (
-          <Button component={NavLink} to="/user">
-            Users
-          </Button>
-        )}
-        {currentUser !== null && currentUser.roles.includes(Role.Moderator) && (
-          <Button component={NavLink} to="/meeting">
-            Meetings
-          </Button>
-        )}
-        {currentUser !== null && (
-          <Button component={NavLink} to="/mymeetings">
-            My meetings
-          </Button>
-        )}
+  const buttonFont = createMuiTheme({
+    typography: {
+      fontFamily: ['Montserrat', 'sans-serif'].join(','),
+    },
+  });
+  const body = (
+    <>
+      <ThemeProvider theme={buttonFont}>
+        <Box>
+          {currentUser !== null && currentUser.roles.includes(Role.Admin) && (
+            <Button component={NavLink} to="/user" className={classes.link}>
+              Users
+            </Button>
+          )}
+          {currentUser !== null && currentUser.roles.includes(Role.Moderator) && (
+            <Button component={NavLink} to="/meeting" className={classes.link}>
+              Meetings
+            </Button>
+          )}
+          {currentUser !== null && (
+            <Button
+              component={NavLink}
+              to="/mymeetings"
+              className={classes.link}
+            >
+              My meetings
+            </Button>
+          )}
 
-        {currentUser === null ? (
-          <Button component={NavLink} to="/signin">
-            Sign in
-          </Button>
-        ) : (
-          <Button
-            onClick={() => {
-              localStorage.removeItem('currentUser');
-              setCurrentUser(null);
-            }}
-            component={NavLink}
-            to="/signin"
-          >
-            Logout
-          </Button>
-        )}
-      </Toolbar>
-    </AppBar>
+          {currentUser === null ? (
+            <Button component={NavLink} to="/signin" className={classes.link}>
+              Sign in
+            </Button>
+          ) : (
+            <Button
+              onClick={() => {
+                localStorage.removeItem('currentUser');
+                setCurrentUser(null);
+              }}
+              component={NavLink}
+              to="/signin"
+              className={classes.link}
+            >
+              Logout
+            </Button>
+          )}
+        </Box>
+      </ThemeProvider>
+    </>
+  );
+  return (
+    <ResponsiveMenu
+      menuOpenButton={<MenuOpenIcon className={classes.icon} />}
+      menuCloseButton={<CloseIcon className={classes.icon} />}
+      changeMenuOn="500px"
+      largeMenuClassName={classes.largeMenu}
+      smallMenuClassName={classes.smallMenu}
+      menu={body}
+    />
+    // <AppBar position="sticky" className={classes.bar}>
+    //   <Toolbar className={classes.toolbar}>
+
+    //   </Toolbar>
+    // </AppBar>
   );
 }
